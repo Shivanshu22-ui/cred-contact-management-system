236f90bfa046fbcd7428992fae041017908561f2
= <=>
2022-11-03 13:27:48 +0530

    trie data structure

--- contactmanager/service/search.cpp
+++ contactmanager/service/search.cpp
@@ -1,123 +1,180 @@
-#include<fstream>
-#include<string.h>
-#include<iostream>
-
+#include <fstream>
+#include <string.h>
+#include <iostream>
 using namespace std;
 
-class search
+class contact
 {
-    char firstName[20],[20],email[30];
     long ph;
+    char firstName[20], lastName[20];
 
 public:
-    void searchFunc(char typeOfSearch[], char searchValue[], char searchField[]){
-        if(typeOfSearch[0] == 'p' || typeOfSearch[0] == 'P'){
-            if(searchField[0] == 'l' || searchField[0] == 'L'){
-                while(fp.read((char*)this,sizeof(contact)))
+
+    void create_contact(long ph,char fname[],char lname[]){
+        this->ph=ph;
+        this->firstName[20]=fname[20];
+        this->lastName[20]=lname[20];
+}
+    void show_contact()
+    {
+        cout << endl
+             << "Phone #: " << ph;
+        cout << endl
+             << "Name: " << firstName << " " << lastName;
+        cout << endl
+             << "Address: " << lastName;
+    }
+
+    long getPhone()
+    {
+        return ph;
+    }
+
+    char *getFirstName()
+    {
+        return firstName;
+    }
+
+    char *getLastName()
+    {
+        return lastName;
+    }
+};
+
+fstream fp;
+contact cont;
+
+void save_contact(long ph, char fname[], char lname[])
+{
+    fp.open("contactBook.txt", ios::out | ios::app);
+    cont.create_contact(ph, fname, lname);
+    fp.write((char *)&cont, sizeof(contact));
+    fp.close();
+    cout << endl
+         << endl
+         << "Contact Has Been Sucessfully Created...";
+    getchar();
+}
+
+void show_all_contacts()
+{
+    cout << "LIST OF CONTACTS" << endl;
+    fp.open("contactBook.txt", ios::in);
+    while (fp.read((char *)&cont, sizeof(contact)))
+    {
+        cont.show_contact();
+        cout << endl
+             << "=================================================\n"
+             << endl;
+    }
+    fp.close();
+}
+
+void display_contact(char typeOfSearch[], char searchField[], char searchValue[])
+{
+    bool found = false;
+    int ch;
+
+    fp.open("contactBook.txt", ios::in);
+    while (fp.read((char *)&cont, sizeof(contact)))
+    {
+
+        if (strcmpi(typeOfSearch, "Partial") == 0)
+        {
+            cont.show_contact();
+        }
+        else
+        {
+            if (strcmpi(searchField, "Phone") == 0)
+            {
+                if(cont.getPhone()== atoi(searchValue))
                 {
-                    if(strcmpi(searchValue,getName())==0)
-                    {
-                        show_contact();
-                    }
+                    cont.show_contact();
+                }
+                {
+                cont.show_contact();
+                found=true;
                 }
             }
-            else if(searchField[0] == 'f' || searchField[0] == 'F'){
-                while(fp.read((char*)this,sizeof(contact)))
+            else if (strcmpi(searchField, "FName") == 0)
+            {
+                if(strcmpi(cont.getFirstName(), searchValue)==0)
                 {
-                    if(strcmpi(searchValue,getAddress())==0)
-                    {
-                        show_contact();
-                    }
+                    cont.show_contact();
+                    found=true;
                 }
+                
             }
-            else if(searchField == '3'){
-                while(fp.read((char*)this,sizeof(contact)))
+            else
+            {
+                if(strcmpi(cont.getLastName(), searchValue)==0)
                 {
-                    if(strcmpi(searchValue,getEmail())==0)
-                    {
-                        show_contact();
-                    }
+                    cont.show_contact();
+                    found=true;
                 }
-            }
-            else if(searchField == '4'){
-                while(fp.read((char*)this,sizeof(contact)))
-                {
-                    if(strcmpi(searchValue,getPhone())==0)
-                    {
-                        show_contact();
-                    }
-                }
+                
             }
         }
-            if(searchField == 'firstName'){
-                while(fp.read((char*)this,sizeof(contact)))
-                {
-                    if(strcmpi(searchValue,getName())==0)
-                    {
-                        show_contact();
-                    }
-                }
-            }
-            else if(searchField == '2'){
-                while(fp.read((char*)this,sizeof(contact)))
-                {
-                    if(strcmpi(searchValue,getAddress())==0)
-                    {
-                        show_contact();
-                    }
-                }
-            }
-            else if(searchField == '3'){
-                while(fp.read((char*)this,sizeof(contact)))
-                {
-                    if(strcmpi(searchValue,getEmail())==0)
-                    {
-                        show_contact();
-                    }
-                }
-            }
-            else if(searchField == '4'){
-                while(fp.read((char*)this,sizeof(contact)))
-                {
-                    if(strcmpi(searchValue,getPhone())==0)
-                    {
-                        show_contact();
-                    }
-                }
-            }
-        
-    };    
+    }
+    fp.close();
+    if (found == false)
+    {
+        cout << "\n\nNo Contact found";
+    }
+    getchar();
+}
 
-        
-        // void search_contact()
-        // {
-        //     char n[20];
-        //     int flag=0;
-        //     cout<<"Enter name to search: ";
-        //     cin.ignore();
-        //     cin>>n;
-        //     fp.open("contact.txt",ios::in);
-        //     while(fp)
-        //     {
-        //         fp>>name>>ph>>add>>email;
-        //         if(strcmp(name,n)==0)
-        //         {
-        //             cout<<"Phone #: "<<ph<<endl;
-        //             cout<<"Name: "<<name<<endl;
-        //             cout<<"Address: "<<add<<endl;
-        //             cout<<"Email Address : "<<email<<endl;
-        //             flag=1;
-        //         }
-        //     }
-        //     fp.close();
-        //     if(flag==0)
-        //         cout<<"Contact not found";
-        // }
-    };
 
-int main()
+
+int main(int argc, char *argv[])
 {
-    search s;
-    s.search_contact();
+    for (;;)
+    {
+        int ch;
+        cout << "\n\t ** Welcome to Contact Management System **";
+        cout << "\n\n\n\t\t\tMAIN MENU\n\t\t=====================\n\t\t[1] Add a new Contact\n\t\t[2] List all Contacts\n\t\t[3] Search for contact\n\t\t[0] Exit\n\t\t=================\n\t\t";
+        cout << "Enter the choice:";
+        cin >> ch;
+
+        switch (ch)
+        {
+        case 0:
+            cout << "\n\n\t\tThank you for using our Library";
+            break;
+
+        case 1:
+        long ph;
+            char fname[20], lname[20];;
+            save_contact(ph, fname, lname);
+            break;
+
+        case 2:
+            show_all_contacts();
+            break;
+        case 3:
+            char searchField[20], typeOfSearch[20], searchValue[20];
+            cout << "\n\n\tSearch Input ";
+            cin >> searchValue;
+            display_contact(typeOfSearch, searchField, searchValue);
+            break;
+
+
+        default:
+            break;
+        }
+
+        int opt;
+        cout << "\n\n\n..::Enter the Choice:\n\n\t[1] Main Menu\t\t[0] Exit\n";
+        cin >> opt;
+
+        switch (opt)
+        {
+        case 1:
+            continue;
+
+        case 0:
+            break;
+        }
+    }
     return 0;
 }
